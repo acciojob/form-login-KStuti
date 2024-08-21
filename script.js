80de5b0e95122a5074892ed1467baef6e5ff10dc
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-08-21 13:45:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
+    
+    const fullName = `${firstName} ${lastName}`;
+    
+    alert(fullName);
 }
