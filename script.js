7f600db54c8572c9151089ea22b5a8fd556995c1
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-08-24 01:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function getFormvalue() {
-    // Get the form element by its id
-    const form = document.getElementById('form1');
-    
-    // Get the values of the first and last name fields using their names
-    const firstName = form.elements['fname'].value;
-    const lastName = form.elements['lname'].value;
+    // Get the form element by its ID
+    const form = document.getElementById('nameForm');
 
-    
-    alert(firstName + " " + lastName);
+    // Retrieve the value of the first and last name inputs
+    const firstName = form.elements['firstName'].value;
+    const lastName = form.elements['lastName'].value;
+
+    // Concatenate the names and show in an alert
+    alert(firstName + " " + lastName);
 }
+
+// Adding an event listener to the form submission to trigger the function
+document.getElementById('nameForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent form from submitting
+    getFormvalue();  // Call the function to get and display the values
+});
